0f11cfb333bbcc862186e461a7b44fa22f78fc3d
mocchantai <mocchantai31415@gmail.com>
2024-11-02 10:20:07 +0900

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+
+const counterStore = useCounterStore()
+</script>
+
+<template>
+  <h1>The Welcome</h1>
+  <button @click="counterStore.increment">Increment</button>
+  <button @click="counterStore.decrement">Decrement</button>
+  <p>Count: {{ counterStore.count }}</p>
+</template>
